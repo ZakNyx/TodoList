7a666940156d93c7293ce1b9ba24c8bf9af2cfef
Zakaria Ihirri <zihirri@e1r9p10.1337.ma>
2023-05-30 20:27:09 +0100

    Adding some css

--- src/Task.js
+++ src/Task.js
@@ -1,14 +1,35 @@
 export const Task = (props) => {
-	return(
+	return (
 		<div
 			className="task"
-			style={{ backgroundColor: props.completed ? "#00cc00C3" : "white",
-			boxShadow: props.completed ? "0 0 4px rgba(0, 128, 0, 0.5)" : "none",
-			transition: "background-color 0.3s ease, box-shadow 0.3s ease"}}	
+			style={{
+				backgroundColor: props.completed ? "#00cc00C3" : "white",
+				boxShadow: props.completed ? "0 0 4px rgba(0, 128, 0, 0.5)" : "none",
+				transition: "background-color 0.3s ease, box-shadow 0.3s ease",
+			}}
 		>
-			<h1>{props.taskName}</h1>
-			<button className="button complete" onClick={() => props.completeTask(props.id)}>{props.completed ? "Undo" : "Complete"}</button>
-			<button className="button delete" onClick={()  => props.deleteTask(props.id)} >Delete</button>
+			<h1 className="taskText">{props.taskName}</h1>
+			{props.completed ? (
+				<button
+					className="button undo"
+					onClick={() => props.completeTask(props.id)}
+				>
+					Undo
+				</button>
+			) : (
+				<button
+					className="button complete"
+					onClick={() => props.completeTask(props.id)}
+				>
+					Complete
+				</button>
+			)}
+			<button
+				className="button delete"
+				onClick={() => props.deleteTask(props.id)}
+			>
+				Delete
+			</button>
 		</div>
 	);
 };
